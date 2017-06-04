00e9deb7c3ff3f4a1a59c87388c602d8eda9e364
rraj-gautam <thunderdfrost@gmail.com>
2017-06-05 00:37:21 +0545

    wifi-option

--- wifi.sh
+++ wifi.sh
@@ -1,17 +1,31 @@
 #!/bin/sh
+#list networks
+#run with a [. ./filename.sh] 
+#inorder to make the variables included within same bash
+#ie.In order to receive environment changes back from the script
+#  
 echo "do you want to list all available connections? y/n"
 read decision1
-#if [$decision="y"]
-#then
+#export decision1
+if [ $decision1 == "y" ]
+#space between operators is needed so that shell doesnot treat the 
+#condition/operation as vairable.
+then
 	nmcli d wifi list
-#elif [$decision="n"] then
-#else
-#	echo"thank you, see you soon"
-#else
-#echo "y/n?"
-#read decision
-#fi
+#elif [ "$decision" == "n" ] ###"$decision" and $decision are same
+#then
+#	echo "thank you"
+else
+	echo "skipped to next step- "
+#	echo "y/n?"
+#	read decision1
+fi
 
-echo".......also list all saved coonections:........? y/n?"
+echo ".......also list all saved coonections:........? y/n?"
 read decision2
-nmcli c
+if [ "$decision2" == "y" ]
+then
+	nmcli c
+else 
+	echo "thank you, see you again"
+fi
