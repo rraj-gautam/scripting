741bf18d8b4f55bf58bd7e623c541b5447b54470
rraj-gautam <thunderdfrost@gmail.com>
2017-06-07 17:17:24 +0545

    connect to new network

--- wifi.sh
+++ wifi.sh
@@ -1,17 +1,50 @@
 #!/bin/sh
+#list networks
+#run with a [. ./filename.sh] 
+#inorder to make the variables included within same bash
+#ie.In order to receive environment changes back from the script
+#  
 echo "do you want to list all available connections? y/n"
 read decision1
-#if [$decision="y"]
-#then
+#export decision1
+if [ $decision1 == "y" ]
+#space between operators is needed so that shell doesnot treat the 
+#condition/operation as vairable.
+then
 	nmcli d wifi list
-#elif [$decision="n"] then
-#else
-#	echo"thank you, see you soon"
-#else
-#echo "y/n?"
-#read decision
+#elif [ "$decision" == "n" ] ###"$decision" and $decision are same
+#then
+#	echo "thank you"
+else
+	break
+#	echo "skipped to next step- "
+#	echo "y/n?"
+#	read decision1
+fi
+
+#echo ".......also list all saved coonections:........? y/n?"
+#read decision2
+#if [ "$decision2" == "y" ]
+#then
+#	nmcli c
+#	ping -c 3 8.8.8.8
+#else 
+#	echo "thank you, see you again"
 #fi
 
-echo".......also list all saved coonections:........? y/n?"
-read decision2
-nmcli c
+echo "enter the ssid you want to connect: "
+read ssidname
+echo "enter the password: "
+read ssidpassword
+
+nmcli d wifi connect $ssidname password $ssidpassword
+
+echo "checking network connection...................................................."
+espeak "checking network connection"
+if (ping -c 3 8.8.8.8) then
+	echo "connected successfully"
+	espeak "connected successfully"
+esle
+	echo "connection failed. please try with correct information"
+	espeak "connection failed. please try with correct information"
+fi
