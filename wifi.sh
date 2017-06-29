56ec4c3676c9c8f1333c86d7244ceeaf6a3ed643
rraj-gautam <thunderdfrost@gmail.com>
2017-06-29 14:09:15 +0545

    updated wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #list networks
-#**************run with a [. ./filename.sh]********** 
+#run with a [. ./filename.sh] 
 #inorder to make the variables included within same bash
 #ie.In order to receive environment changes back from the script
 #  
@@ -16,16 +16,42 @@ then
 #then
 #	echo "thank you"
 else
-	echo "skipped to next step- "
+	break
+#	echo "skipped to next step- "
 #	echo "y/n?"
 #	read decision1
 fi
 
-echo ".......also list all saved coonections:........? y/n?"
-read decision2
-if [ "$decision2" == "y" ]
-then
-	nmcli c
-else 
-	echo "thank you, see you again"
+#echo ".......also list all saved coonections:........? y/n?"
+#read decision2
+#if [ "$decision2" == "y" ]
+#then
+#	nmcli c
+#	ping -c 3 8.8.8.8
+#else 
+#	echo "thank you, see you again"
+#fi
+
+echo "enter the ssid you want to connect: "
+read ssidname
+echo "enter the password: "
+read ssidpassword
+
+nmcli d wifi connect $ssidname password $ssidpassword
+
+echo "checking network connection...................................................."
+espeak "checking network connection"
+if (ping -c 3 8.8.8.8) then
+	echo "connected successfully" $ssidname
+	espeak "connected successfully" $ssidname
+
+else
+	echo "connection failed. please try with correct information"
+	espeak "connection failed. please try with correct information"
+
+
+	echo "trying for saved connection......"
+	nmcli d connect wlp9s0 
 fi
+
+
