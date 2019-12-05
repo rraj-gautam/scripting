3a02833d51f46f66f1929fddd01e54d42cbfdf90
rraj gautam <thunderdfrost@gmail.com>
2019-12-05 13:25:36 +0545

    Update openPortsFirewall.sh

--- openPortsFirewall.sh
+++ openPortsFirewall.sh
@@ -4,3 +4,8 @@ do
 firewall-cmd --add-port=$i/tcp --permanent
 done < ports.txt
 firewall-cmd --reload
+
+
+##### rich-rule #####
+## firewall-cmd --permanent --zone=public --add-rich-rule='rule family="ipv4" source address="10.13.222.55" port protocol="udp" port="161" accept'
+## firewall-cmd --permanent --zone=public --add-rich-rule='rule family="ipv4" source address="10.13.212.2" port protocol="tcp" port="22" accept'
